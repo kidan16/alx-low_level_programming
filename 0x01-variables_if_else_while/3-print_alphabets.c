790ac4083bacd992bba5439f68c0fb4730db41d0
kidan16 <eyueltekle@hotmail.com>
2022-03-12 08:13:32 -0800

    c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,13 +9,13 @@ int main(void)
 char ch1;
 char ch2;
 
-for (ch1 = 'a' ; ch1 <= 'z' ; ch1++);
+for (ch1 = 'a'; ch1 <= 'z'; ch1++);
 
 {
 putchar(ch1);
 }
 
-for (ch2 = 'A' ; ch2 <= 'Z' ; ch2++);
+for (ch2 = 'A'; ch2 <= 'Z'; ch2++);
 
 {
 putchar(ch2);
