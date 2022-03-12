a8f1a4129632a2f17c742d8f764c55a284f5ec15
kidan16 <eyueltekle@hotmail.com>
2022-03-12 07:32:27 -0800

    c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char ch1, ch2;
+
+for (ch1 = 'a' ; ch1 <= 'z' ; ch1++);
+
+{
+putchar(ch1);
+}
+
+for (ch2 = 'A' ; ch2 <= 'Z' ; ch2++);
+
+{
+putchar(ch2);
+}
+
+putchar('\n');
+
+return (0);
+}
