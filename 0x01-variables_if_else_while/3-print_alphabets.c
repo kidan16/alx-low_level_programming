fa3272d4d4d29b922e11eb5046e739c84f3b9987
kidan16 <eyueltekle@hotmail.com>
2022-03-13 10:40:36 -0700

    c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,4 +1,4 @@
-s#include <stdio.h>
+#include <stdio.h>
 /**
  * main - Entry point
  *
@@ -10,12 +10,14 @@ int main(void)
 char lower_case;
 char upper_case;
 
-for (lower_case = 'a'; lower_case <= 'z'; lower_case++);
+for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+
 {
 putchar(lower_case);
 }
 
-for (upper_case = 'A'; upper_case <= 'Z'; upper_case++);
+for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
+
 {
 putchar(upper_case);
 }
