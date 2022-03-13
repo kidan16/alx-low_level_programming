0c2ce21c31c2e50cf88fa138ec35e272f4cc9fa1
kidan16 <eyueltekle@hotmail.com>
2022-03-13 12:56:16 -0700

    c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+
+{
+char ch;
+
+for (ch = 48; ch <= 57; ch++)
+{
+putchar (ch);
+}
+
+putchar ('\n');
+
+return (0);
+}
