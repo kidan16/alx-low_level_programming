93553ea964a3d130e4eca3c9b026ac15b6cf5103
kidan16 <eyueltekle@hotmail.com>
2022-03-13 13:55:10 -0700

    c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+
+{
+int a;
+
+for (a = 48; a <= 57; a++)
+{
+putchar (a);
+putchar (',');
+putchar ('\t');
+}
+putchar ('\n');
+
+return (0);
+}
