2fe200d17d315fecf7245982437fdad5cd232445
kidan16 <eyueltekle@hotmail.com>
2022-03-14 13:28:57 -0700

    c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+int x;
+int y;
+int z;
+
+for (x = 48; x <= 57; x++)
+{
+for (y = 48; y <= 57; y++)
+{
+for (z = 48; z <= 57; z++)
+{
+if (((x != y) && (x != z) && (y != z)) && (x != y) && (y != z))
+{
+putchar(x);
+putchar(y);
+putchar(z);
+
+if ((x == 55) && (y == 56) && (z == 57))
+{}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+
+return (0);
+}
