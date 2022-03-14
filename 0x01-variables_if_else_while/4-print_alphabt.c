044d2b824053e2e8c6b7212b7b293d3b460fc379
kidan16 <eyueltekle@hotmail.com>
2022-03-14 15:39:24 -0700

    c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,11 +10,19 @@ int main(void)
 {
 char ch;
 
-if (ch = 'a' ; ch <= 'z' ; ch++ || ch != 'e' || ch != 'q')
+for (ch = 'a' ; ch <= 'z' ; ch++)
 {
+if (ch == 'e')
+
+continue;
+
+else if (ch == 'q')
+
+continue;
+
 putchar(ch);
+
 }
-else{} 
 
 putchar('\n');
 
