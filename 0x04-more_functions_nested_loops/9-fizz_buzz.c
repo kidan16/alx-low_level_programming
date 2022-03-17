f3cd605b1d49758b3f048d84f488ade7d2d9ac5e
kidan16 <99591139+kidan16@users.noreply.github.com>
2022-03-18 01:55:10 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,38 +5,39 @@
  */
 int main(void)
 {
-	int i;
+int i;
 
-	for (i = 1; i < 101; ++i)
-	{
-		if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz ");
-		}
-		else
-		{
-			if (i % 3 == 0)
-			{
-			printf("Fizz ");
-			}
-			else if (i % 5 == 0)
-			{
-				if (i == 100)
-				{
-					printf("Buzz");
-				}
-				else
-				{
-					printf("Buzz ");
-				}
-			}
-			else
-			{
-				printf("%i ", i);
-			}
-		}
+for (i = 1; i < 101; ++i)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+else
+{
+if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (i % 5 == 0)
+{
+if (i == 100)
+{
+printf("Buzz");
+}
+else
+{
+printf("Buzz ");
+}
+}
+else
+{
+printf("%i ", i);
+}
 
-	}
-	printf("\n");
-	return (0);
+}
+
+}
+printf("\n");
+return (0);
 }
