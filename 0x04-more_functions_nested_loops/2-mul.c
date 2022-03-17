3b6bf8b2142f67e18290e69965f72e03284c1191
kidan16 <99591139+kidan16@users.noreply.github.com>
2022-03-17 22:13:48 +0300

    Update 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul.c - multiplies two integers
+ * @a,b: intiger parameter
+ * Return: product of a & b
+ */
+int mul(int a, int b)
+
+{
+int product;
+
+product = a*b;
+
+return product;
+}
