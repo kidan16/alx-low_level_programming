c89c3789944a48c598995499e7cab4a0ca035ba4
kidan16 <99591139+kidan16@users.noreply.github.com>
2022-03-18 19:20:28 +0300

    Create 101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,16 @@
+void printnumber(int n)
+{
+
+    if (n < 0) {
+        _putchar('-');
+        n = -n;
+    }
+
+    if (n == 0)
+        _putchar('0');
+
+    if (n/10)
+        printnumber(n/10);
+
+    _putchar(n%10 + '0');
+}
