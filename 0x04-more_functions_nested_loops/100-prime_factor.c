ca83a47773e44eaad45e6c4cd3e5b52bc670d10b
“kidan16” <eyueltekle@hotmail.com>
2022-03-18 18:26:11 +0300

    c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+long long int number = 612852475143, large_factor = 0, i = 2;
+while (number != 1)
+{
+if (number % i == 0)
+{
+while (number % i == 0)
+{
+number / = i;
+}
+large_factor = i;
+}
+i++;
+}
+printf("%lld\n\n", large_factor);
+return (0);
+}
