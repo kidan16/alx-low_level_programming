91516f5f9a65eac3f8e953d123a5e6eec27ea9ff
“kidan16” <eyueltekle@hotmail.com>
2022-03-21 14:23:20 +0300

    pointers

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  * _strlen -returns the length of a string.
  * @s: is the value of the string variable.
@@ -10,7 +11,8 @@ int _strlen(char *s)
 {
 	char string;
 	int length;
-
+	
+	string [] = *s;
 	length = strlen(string);
 
 	return (length);
