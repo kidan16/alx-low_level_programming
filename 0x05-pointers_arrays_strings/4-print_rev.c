8185e27f6150eb889be6038cb15b4100b4051420
kidan16 <99591139+kidan16@users.noreply.github.com>
2022-03-21 18:39:55 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,57 +1,24 @@
-<<<<<<< HEAD
 #include "main.h"
 
 /**
- * rev_string - reverse a string.
+ * print_rev - print a string in reverse.
  *
- *@s: the string to be reversed.
+ *@s: the string to print in reverse.
+ *
  */
 
-void rev_string(char *s)
+void print_rev(char *s)
 {
-	int length, j, i;
-	char v1, v2;
+	int i = 0;
+	int length;
 
 	for (length = 0; s[length] != '\0'; length++)
 	{
 	}
 
-	j = length - 1;
-	i = 0;
-
-	while (j > i)
+	for (i = length - 1; i >= 0; i--)
 	{
-			v1 = s[i];
-			v2 = s[j];
-			s[i] = v2;
-			s[j] = v1;
-			j--;
-			i++;
+		_putchar(s[i]);
 	}
+	_putchar('\n');
 }
-}
-=======
-#include "holberton.h"
-/**
- * print_rev -prints a string, in reverse, followed by a new line.
- * @s: char pointer.
- * Return: void.
- */
-void print_rev(char *s)
-
-{
-int i, n;
-
-for (i = 0; s[i] != '\0'; i++)
-
-continue;
-} 
-n = i - 1;
-
-for (i = n; i >= 1; i--)
-{
-_putchar(s[i]);
-}
-_putchar('\n');
-}
->>>>>>> d74d659b39cad152c65debda348ef2ba74dfd9ed
