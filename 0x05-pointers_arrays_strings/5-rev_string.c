d1fd441aa75f71b72fcb2d5cfa9166fcae8a782b
kidan16 <99591139+kidan16@users.noreply.github.com>
2022-03-21 17:41:01 +0300

    Update 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * print_rev -reverses a string.
+ * @s: char pointer.
+ * Return: void.
+ */
+void rev_string(char *s)
+
+{
+int i, j;
+char a[500];
+i = 0;
+j = 0;
+while(*(s+1))
+{
+*(a + i) = *(s + i);
+i++;
+}
+
+i = i - 1;
+while(i >= 0)
+{
+*(s + i) = *(a + j);
+j++;
+i--;
+}
