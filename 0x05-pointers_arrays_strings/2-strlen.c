a070e1e872e05ad135536ac2f5fee8601229bc6c
“kidan16” <eyueltekle@hotmail.com>
2022-03-21 14:18:17 +0300

    pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen -returns the length of a string.
+ * @s: is the value of the string variable.
+ *
+ * Return: void.
+ */
+int _strlen(char *s)
+
+{
+	char string;
+	int length;
+
+	length = strlen(string);
+
+	return (length);
+}
