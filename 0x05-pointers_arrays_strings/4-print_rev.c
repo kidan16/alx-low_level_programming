0a75b6e4fdc894285a554a803be96eabaccba0bf
“kidan16” <eyueltekle@hotmail.com>
2022-03-21 16:58:14 +0300

    pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev -prints a string, in reverse, followed by a new line.
+ * @s: char pointer.
+ *
+ * Return: void.
+ */
+void print_rev(char *s)
+
+{
+	int i, n;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		continue;
+	}
+
+	n = i - 1;
+	for (i = n; i >= 1; i--)
+	{
+	_putchar(s[i]);
+	}
+
+	_putchar('\n');
+}
