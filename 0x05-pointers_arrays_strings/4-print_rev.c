dea0f860e45140f2ecd486bfa1b45759dc91aa4c
kidan16 <99591139+kidan16@users.noreply.github.com>
2022-03-21 17:11:48 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,18 +8,17 @@
 void print_rev(char *s)
 
 {
-        int i, n;
+int i, n;
 
-        for (i = 0; s[i] != '\0'; i++)
-        {
-                continue;
-        }
+for (i = 0; s[i] != '\0'; i++)
+{
+continue;
+} 
+n = i - 1;
 
-        n = i - 1;
-        for (i = n; i >= 1; i--)
-        {
-        _putchar(s[i]);
-        }
-
-        _putchar('\n');
+for (i = n; i >= 1; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
 }
