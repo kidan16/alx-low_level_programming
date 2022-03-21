0528f2b7403caef572fc0d7ffb67efc1a14fcf76
kidan16 <99591139+kidan16@users.noreply.github.com>
2022-03-21 17:26:15 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,7 +2,6 @@
 /**
  * print_rev -prints a string, in reverse, followed by a new line.
  * @s: char pointer.
- *
  * Return: void.
  */
 void print_rev(char *s)
@@ -11,7 +10,7 @@ void print_rev(char *s)
 int i, n;
 
 for (i = 0; s[i] != '\0'; i++)
-{
+
 continue;
 } 
 n = i - 1;
