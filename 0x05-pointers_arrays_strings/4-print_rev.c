78564a631102ffcabe1a4b8d6eb2455b44381425
“kidan16” <eyueltekle@hotmail.com>
2022-03-22 05:56:04 +0300

    c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,14 +6,14 @@
  */
 void print_rev(char *s)
 {
-int i = 0;
-int length;
-for (length = 0; s[length] != '\0'; length++)
-{
-}
-for (i = length - 1; i >= 0; i--)
-{
-_putchar(s[i]);
-}
-_putchar('\n');
+	int i = 0;
+	int length;
+	for (length = 0; s[length] != '\0'; length++)
+	{
+	}
+	for (i = length - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
 }
