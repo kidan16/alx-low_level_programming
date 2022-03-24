634ce3267a5389358c89f7ceb37ac4fb17ccc3b5
“kidan16” <eyueltekle@hotmail.com>
2022-03-24 13:18:26 +0300

    pointers

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,23 +9,23 @@
 char *_strcat(char *dest, char *src, int n)
 
 {
-	int l, j;
+int l, j;
 
-	l = 0;
-	while (dest[l] != '\0')
-	{
-		l++;
-	}
+l = 0;
+while (dest[l] != '\0')
+{
+l++;
+}
 
-	j = 0;
-	while (j < n && src[j] != '\0')
-	{
-		dest[l] = src[j];
-		l++;
-		j++;
-	}
+j = 0;
+while (j < n && src[j] != '\0')
+{
+dest[l] = src[j];
+l++;
+j++;
+}
 
-	dest[l] = '\0';
+dest[l] = '\0';
 
-	return (dest);
+return (dest);
 }
