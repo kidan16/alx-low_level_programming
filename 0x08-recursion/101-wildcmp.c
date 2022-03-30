ac589e4d5150a946c296508b332313cf492a16de
“kidan16” <eyueltekle@hotmail.com>
2022-03-30 17:55:49 +0300

    c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * Compare two strings allowing for wildcard char
+ * wildcmp- compare two strings allowing for wildcard char
  * @s1: String being compared
  * @s2: String being compared against
  *
  * Return: 1 if considered identical, 0 otherwise
  */
- int wildcmp(char *s1, char *s2)
+int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
