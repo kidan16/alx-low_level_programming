f10133629703869bb3d1c2b469ac13d5d986dec7
“kidan16” <eyueltekle@hotmail.com>
2022-04-01 12:49:55 +0300

    c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - function
+ * @argc: an int parameter
+ * @argv: a string parameter
+ * Return: 0
+ */
+
+int main(int argc, char* argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+
+	return (0);
+}
