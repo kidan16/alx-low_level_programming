c217d9a6d6c9d643024798c348b5d72f37638b4a
“kidan16” <eyueltekle@hotmail.com>
2022-04-11 23:12:28 +0300

    c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,12 +12,14 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog *new_dog;
+	struct dog *new;
 
-	new_dog = malloc(sizeof(struct dog));
-	if (new_dog == NULL)
+	new = malloc(sizeof(struct dog));
+	if (new == NULL)
 		return (NULL);
-	new_dog->name = name;
-	new_dog->owner = owner;
-	return (new_dog);
+	new->name = name;
+	new->age = age;
+	new->owner = owner;
+
+	return (new);
 }
