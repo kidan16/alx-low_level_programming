a55448693bc83977a4adfb20dee249da7b15ab3a
“kidan16” <eyueltekle@hotmail.com>
2022-04-11 22:50:12 +0300

    c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,7 +18,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (new_dog == NULL)
 		return (NULL);
 	new_dog->name = name;
-	new_dog->age = age;
 	new_dog->owner = owner;
 	return (new_dog);
 }
