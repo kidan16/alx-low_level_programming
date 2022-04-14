9b1e60347a2bc59f61c0f8b82e986cf812396771
“kidan16” <eyueltekle@hotmail.com>
2022-04-14 05:46:05 +0300

    function_pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,11 @@
 #include "function_pointers.h"
+
 /**
  * print_name- this functions prints a name
-(* a blank line
- *@name: string to print.
- *@f: this a function pointer, that recieves a string.
-* Description: this function prints a name)?
-(* section header: the header of this function is function_pointers.h)*
-* Return: this is a void function no return
-*/
+ * @name: string to print.
+ * @f: this a function pointer, that recieves a string.
+ * Return: null
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
