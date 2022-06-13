c4d903a6767b152fbf89c671da12434c50a70fef
kidan16 <eyueltekle@hotmail.com>
2022-06-13 17:44:07 +0300

    ynamic_libraries

---
+++ 0x18-dynamic_libraries/_strncat.c
@@ -0,0 +1,30 @@
+/**
+ * _strncat- concatenates two strings taking n bytes from src.
+ * @dest: is a string parameter.
+ * @src: is a string parameter.
+ * @n: is an int parameter.
+ * Return: char.
+ */
+char *_strncat(char *dest, char *src, int n)
+
+{
+int l, j;
+
+l = 0;
+while (dest[l] != '\0')
+{
+l++;
+}
+
+j = 0;
+while (j < n && src[j] != '\0')
+{
+dest[l] = src[j];
+l++;
+j++;
+}
+
+dest[l] = '\0';
+
+return (dest);
+}
